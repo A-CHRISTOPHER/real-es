8821c5ff202cf008515cb1319df5763c036e04a4
LABS\placement <sblock>
2024-07-03 19:50:28 +0530

    my-new

---
+++ real-estate/src/App.jsx
@@ -0,0 +1,14 @@
+import "./Layout.scss"
+import Navbar from "./components/navbar/Navbar.jsx"
+
+function App() {
+    return (
+    <>
+      <div className="Layout">
+        <Navbar />
+      </div>
+    </>
+  )
+}
+
+export default App
